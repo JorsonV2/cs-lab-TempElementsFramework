04734c25d4a8f813b47370c94fb4d7f575b296bd
JorsonV2 <38647517+JorsonV2@users.noreply.github.com>
2021-01-10 13:37:02 +0100

    Create Project

---
+++ TempElementsFramework/TempElementsLib/TempFile.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using TempElementsLib.Interfaces;
+
+namespace TempElementsLib
+{
+    public class TempFile : ITempFile
+    {
+        private bool disposedValue;
+
+        public TempFile()
+        {
+        }
+
+        public string FilePath => throw new NotImplementedException();
+
+        public bool IsDestroyed => throw new NotImplementedException();
+
+        protected virtual void Dispose(bool disposing)
+        {
+            if (!disposedValue)
+            {
+                if (disposing)
+                {
+                    // TODO: Wyczyścić stan zarządzany (obiekty zarządzane)
+                }
+
+                // TODO: Zwolnić niezarządzane zasoby (niezarządzane obiekty) i przesłonić finalizator
+                // TODO: Ustawić wartość null dla dużych pól
+                disposedValue = true;
+            }
+        }
+
+        // // TODO: Przesłonić finalizator tylko w sytuacji, gdy powyższa metoda „Dispose(bool disposing)” zawiera kod służący do zwalniania niezarządzanych zasobów
+        // ~TempFile()
+        // {
+        //     // Nie zmieniaj tego kodu. Umieść kod czyszczący w metodzie „Dispose(bool disposing)”.
+        //     Dispose(disposing: false);
+        // }
+
+        public void Dispose()
+        {
+            // Nie zmieniaj tego kodu. Umieść kod czyszczący w metodzie „Dispose(bool disposing)”.
+            Dispose(disposing: true);
+            GC.SuppressFinalize(this);
+        }
+    }
+}
